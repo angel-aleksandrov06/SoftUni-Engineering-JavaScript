d188fadf5093cd8f84cd036121d87fe980f1cc93
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-01-15 21:20:04 +0200

    Update 1.fruit.js

--- JS Advanced/Labs and Exercises/ExerciseSyntaxFunctionsAndStatements/1.fruit.js
+++ JS Advanced/Labs and Exercises/ExerciseSyntaxFunctionsAndStatements/1.fruit.js
@@ -1,10 +1,8 @@
-function byFruit(type, grams, price){
+function byFruit(type, grams, price) {
+
     let weight = grams / 1000;
     let fruit = type;
     let money = price * weight;
 
     console.log(`I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`)
 }
-
-byFruit('orange', 2500, 1.80);
-byFruit('apple', 1563, 2.35);
