614d110de3743d74d0ac9c3394375db6640d0995
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-01-16 22:56:31 +0200

    Create 5.calorieObject.js

---
+++ JS Advanced/Labs and Exercises/ExerciseSyntaxFunctionsAndStatements/5.calorieObject.js
@@ -0,0 +1,11 @@
+function calorieObject (elements){
+    let result = {};
+
+    for (let i = 0; i < elements.length; i+=2) {
+        result[elements[i]] = parseInt(elements[i+1])
+    }
+
+    console.log(result);
+}
+
+calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
