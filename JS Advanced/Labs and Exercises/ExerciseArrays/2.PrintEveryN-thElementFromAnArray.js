ff2b8c19a67f9a6960fe040eec518c82a107e231
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-01-17 14:49:11 +0200

    Create 2.PrintEveryN-thElementFromAnArray.js

---
+++ JS Advanced/Labs and Exercises/ExerciseArrays/2.PrintEveryN-thElementFromAnArray.js
@@ -0,0 +1,16 @@
+function solution(input) {
+    let step = +input.pop();
+
+    for (let index = 0; index < input.length; index+=step) {
+        console.log(input[index])
+        
+    }
+}
+
+solution(['5', 
+'20', 
+'31', 
+'4', 
+'20', 
+'2']
+);
