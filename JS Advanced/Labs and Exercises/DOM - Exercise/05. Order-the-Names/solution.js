d9ed0154d1628e71e67d544440bd60d247a1025f
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-02-04 16:04:42 +0200

    add DOM - Exercies { 05, 06 }
    05. Order-the-Names \ solutin.js
    06. Table-Search-Engine \ solution.js

--- JS Advanced/Labs and Exercises/DOM - Exercise/05. Order-the-Names/solution.js
+++ JS Advanced/Labs and Exercises/DOM - Exercise/05. Order-the-Names/solution.js
@@ -1,4 +1,24 @@
 function solve() {
+    let registerButton = document.getElementsByTagName("button")[0];
+    let input = document.getElementsByTagName("input")[0];
+    let orderedList = document.getElementsByTagName("ol")[0];
+    
+    registerButton.addEventListener("click", (e) => {
+        let currentInput = input.value.toUpperCase();
+        let refactoredInput = currentInput[0];
 
-    //TODO...
+        for (let i = 1; i < currentInput.length; i++) {
+            refactoredInput+=currentInput[i].toLowerCase();
+        }
+
+        let asciiCodeOfFirstcharInCurrentInput = currentInput.charCodeAt(0);
+        let child = orderedList.getElementsByTagName("li")[asciiCodeOfFirstcharInCurrentInput-65];
+
+        if(child.innerHTML.length > 0) {
+            let currentChildText = child.innerHTML;
+            refactoredInput = currentChildText + ", " + refactoredInput;
+        }
+        child.innerHTML = refactoredInput;
+        input.value = "";
+    })
 }
--- JS Advanced/Labs and Exercises/DOM - Exercise/06. Table-Search-Engine/solution.js
+++ JS Advanced/Labs and Exercises/DOM - Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-   //TODO...
+    let input = document.getElementById("searchField");
+    let button = document.getElementById("searchBtn");
+    let table = document.getElementsByTagName("tbody")[0];
+
+    button.addEventListener("click", (e) => {
+        let inputString = input.value;
+        let children = table.children;
+
+        if(inputString) {
+
+            Array.from(children).forEach(tr => tr.classList.remove("select"));
+            Array.from(children).forEach(tr => {
+                if (tr.textContent.includes(inputString)){
+                    tr.classList.add("select");
+                }
+            })
+        }
+        
+        input.value = "";
+    })
 }
