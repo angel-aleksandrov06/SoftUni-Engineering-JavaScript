569a18259d6c6a8cf312f104e06f231835698b28
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-02-16 15:07:49 +0200

    01. Christmas Gifts Delivery - Done
    01. Christmas Gifts Delivery  100/100

--- JS Advanced/Exams/Retake Exam - 10 December 2019/01. Christmas Gifts Delivery/app.js
+++ JS Advanced/Exams/Retake Exam - 10 December 2019/01. Christmas Gifts Delivery/app.js
@@ -1,3 +1,67 @@
 function solution() {
-    //TO DO
+    let button = document.getElementsByTagName("button")[0];
+    let input = document.getElementsByTagName("input")[0];
+    let listOfGifts = document.getElementsByTagName("ul")[0];
+
+    button.addEventListener("click", (e) => {
+        let newLi = document.createElement("li");
+        let sendButton = document.createElement("button");
+        let deleteButton = document.createElement("button");
+
+        sendButton.textContent = "Send";
+        sendButton.id = "sendButton";
+        sendButton.addEventListener("click", sendItem)
+        newLi.textContent = input.value;
+        newLi.appendChild(sendButton);
+
+        deleteButton.textContent = "Discard";
+        deleteButton.id = "discardButton";
+        deleteButton.addEventListener("click", discardItem);
+        newLi.appendChild(deleteButton);
+        newLi.classList.add("gift");
+        listOfGifts.appendChild(newLi);
+
+        sort(listOfGifts);
+        input.value = "";
+
+        function sendItem() {
+            let sentGiftsUl = document.getElementsByTagName("ul")[1];
+            let li = this.parentNode;
+            let movingLi = document.createElement("li");
+            movingLi.textContent = li.textContent.replace("SendDiscard","");
+            movingLi.classList.add("gift");
+            let ul = this.parentNode.parentNode;
+            ul.removeChild(li);
+            sentGiftsUl.appendChild(movingLi);
+        }
+
+        function discardItem() {
+            let discardedGiftsUl = document.getElementsByTagName("ul")[2];
+            let li = this.parentNode;
+            let movingLi = document.createElement("li");
+            movingLi.textContent = li.textContent.replace("SendDiscard","");
+            movingLi.classList.add("gift");
+            let ul = this.parentNode.parentNode;
+            ul.removeChild(li);
+            discardedGiftsUl.appendChild(movingLi);
+        }
+    })
+
+    function sort(list) {
+        var items = list.childNodes;
+        var itemsArr = [];
+        for (var i in items) {
+            if (items[i].nodeType == 1) { 
+                itemsArr.push(items[i]);
+            }
+        }
+        itemsArr.sort(function (a, b) {
+            return a.innerHTML == b.innerHTML
+                ? 0
+                : (a.innerHTML > b.innerHTML ? 1 : -1);
+        });
+        for (i = 0; i < itemsArr.length; ++i) {
+            list.appendChild(itemsArr[i]);
+        }
+    }
 }
