c5d6a051ba22aa4a4afc1b6f4d399eb55a0bbd92
Angel Aleksandrov <48453153+angel-aleksandrov06@users.noreply.github.com>
2020-03-12 21:30:48 +0200

    add JS Applications Exercises: This
    01. Company.js 100/100

---
+++ JS Applications/Labs and Exercises/Exercise This/01.company.js
@@ -0,0 +1,83 @@
+class Company {
+
+    constructor() {
+        this.realDepartments = {};
+    }
+
+    addEmployee(username, salary, position, department) {
+        if (!username || !salary && salary !== 0 || !position || !department) {
+            throw new Error("Invalid input!");
+        }
+
+        if (salary < 0) {
+            throw new Error(" Invalid input!");
+        }
+
+        if (!this.realDepartments[department]) {
+            this.realDepartments[department] = [
+                {
+                    username,
+                    salary,
+                    position
+                }
+            ]
+        }
+        else {
+            this.realDepartments[department].push({
+                username,
+                salary,
+                position
+            })
+        }
+
+        return `New employee is hired. Name: ${username}. Position: ${position}`;
+    }
+
+    getHighestAvgSalary(departments) {
+        let highestAvgSaleryDepartment = Object.keys(this.realDepartments)
+            .sort((a, b) => {
+                let avgADepartment = this.realDepartments[a].reduce((acc, e) => acc + Number(e.salary), 0) /this.realDepartments[a].length;
+                let avgBDepartment = this.realDepartments[b].reduce((acc, e) => acc + Number(e.salary), 0) /this.realDepartments[b].length;
+
+                return avgBDepartment - avgADepartment;
+            })[0];
+        
+            let highestAvg = this.realDepartments[highestAvgSaleryDepartment].reduce((acc, e) => acc + Number(e.salary), 0) /this.realDepartments[highestAvgSaleryDepartment].length;
+
+        return {
+            highestAvgSaleryDepartment,
+            highestAvg
+        }
+    }
+
+    bestDepartment() {
+        let highestAvg = this.getHighestAvgSalary(this.realDepartments);
+
+        let sb = "";
+        sb+= `Best Department is: ${highestAvg.highestAvgSaleryDepartment}\n`;
+        sb+= `Average salary: ${highestAvg.highestAvg.toFixed(2)}\n`;
+
+        this.realDepartments[highestAvg.highestAvgSaleryDepartment].sort((a,b) => {
+
+            if(a.salary === b.salary){
+                return a.username.localeCompare(b.username)
+            }
+
+            return b.salary - a.salary;
+        }).forEach(element => {
+            sb+= `${element.username} ${element.salary} ${element.position}\n`
+        });
+
+        return sb.trim();
+    }
+}
+
+let c = new Company();
+c.addEmployee("Stanimir", 2000, "engineer", "Construction");
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
+console.log(c.bestDepartment());
